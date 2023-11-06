1fca0995176374afc04868b6e65c52e887603ef4
Dylan Elias <92413592+honeydill@users.noreply.github.com>
2023-11-06 15:26:19 -0600

    Created simple movement and environment

---
+++ Game Music Jam 2023/player/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody3D
+
+var MAX_SPEED = 2;
+
+func _physics_process(delta):
+	
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * MAX_SPEED
+		velocity.z = direction.z * MAX_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, MAX_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MAX_SPEED)
+	
+	move_and_slide()
